44ae80af62598fb766e3480ace4a48c19cde4ffb
cwmk9999@naver.com <>
2026-04-09 12:20:43 +0900

    로그인 회원가입 찐막

--- charlie/src/main/java/Emp/Emplogin.java
+++ charlie/src/main/java/Emp/Emplogin.java
@@ -10,11 +10,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/charlie")
 public class Emplogin extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("kjashfdjksahd");
+		
+		String mod = request.getParameter("mod");
+		System.out.println("찰리"+mod);
+		if("signin".equals(mod)) {
+			System.out.println("charlie회원가입보내기");
+			request.getRequestDispatcher("/WEB-INF/views/emp/emp_signin.jsp").forward(request, response);	
+		} else {
+			System.out.println("charlie로그인으로보내기");
 		request.getRequestDispatcher("/WEB-INF/views/emp/login.jsp").forward(request, response);
+		}
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 	}
 
