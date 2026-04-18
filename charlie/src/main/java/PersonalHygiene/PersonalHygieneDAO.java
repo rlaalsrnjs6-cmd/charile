d48576061878f29f90eb535b0c3286160202dbef
cwmk9999@naver.com <>
2026-04-18 21:00:49 +0900

    위생

--- charlie/src/main/java/PersonalHygiene/PersonalHygieneDAO.java
+++ charlie/src/main/java/PersonalHygiene/PersonalHygieneDAO.java
@@ -12,10 +12,12 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
+import Defective.DefectiveDTO;
 import WorkOrder.WorkOrderDTO;
+import fileLibrary.CommonDTO;
 
 public class PersonalHygieneDAO {
-	public List<PersonalHygieneDTO> select(PersonalHygieneDTO dto) {
+	public List<PersonalHygieneDTO> select(PersonalHygieneDTO dto, CommonDTO pageing) {
 		List<PersonalHygieneDTO> list = new ArrayList();
 
 		Connection conn = null;
@@ -26,20 +28,25 @@ public class PersonalHygieneDAO {
 			Context ctx = new InitialContext();
 
 			DataSource dataFactory = (DataSource) ctx.lookup("java:/comp/env/jdbc/charlie");
-//			System.out.println("DAOMODselect:"+dto.getMod());
 			conn = dataFactory.getConnection();
-			String query = "SELECT * FROM personal_hygiene h ";
-					query += "LEFT OUTER JOIN emp e ";
-					query += "on(h.empno = e.empno)";
+			String query = "SELECT * from ( "
+							+ "SELECT rownum as rnum, subqry.* from ( "
+							+ 		"select * from personal_hygiene ";
 
-			if(dto.getPh_num() != -1) {	
+			if(dto.getPh_num() != -1) {
 				query += "where ph_num = ?";
 			}
+			query +=") subqry) "
+					+ "WHERE rnum >= ? AND rnum <= ?";
+			//�닔�젙
 			ps = conn.prepareStatement(query);
-			if(dto.getPh_num() != -1) {	
-				ps.setInt(1, dto.getPh_num());
+			int idx = 1;
+			if(dto.getPh_num() != -1) {
+				
+				ps.setInt(idx++, dto.getPh_num());
 			}
-			
+			ps.setInt(idx++, pageing.getStart());
+			ps.setInt(idx++, pageing.getEnd());
 			rs = ps.executeQuery();
 
 			while (rs.next()) {
@@ -51,7 +58,7 @@ public class PersonalHygieneDAO {
 				String memo = rs.getString("memo");
 				String supervisor_chk = rs.getString("supervisor_chk");
 				int empno = rs.getInt("empno");
-
+				
 				DTO.setPh_num(ph_num);
 				DTO.setBody_temper(body_temper);
 				DTO.setRegist_time(regist_time);
@@ -61,8 +68,77 @@ public class PersonalHygieneDAO {
 				DTO.setEmpno(empno);
 				list.add(DTO);
 			}
-			System.out.println("hygieneDAO:"+list);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return list;
+	}
+		
+	public List<PersonalHygieneDTO> selectall(PersonalHygieneDTO dto) {
+		List<PersonalHygieneDTO> list = new ArrayList();
+		
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		try {
+			Context ctx = new InitialContext();
 			
+			DataSource dataFactory = (DataSource) ctx.lookup("java:/comp/env/jdbc/charlie");
+			conn = dataFactory.getConnection();
+			String query = "SELECT * from personal_hygiene ";
+			if(dto.getPh_num() != -1) {
+				query += "where ph_num = ?";
+			}
+			ps = conn.prepareStatement(query);
+			
+			if(dto.getPh_num() != -1) {
+				ps.setInt(1,dto.getPh_num());
+			}
+			rs = ps.executeQuery();
+			
+			while (rs.next()) {
+				PersonalHygieneDTO DTO = new PersonalHygieneDTO();
+				int ph_num = rs.getInt("ph_num");
+				double body_temper = rs.getDouble("body_temper");
+				Date regist_time = rs.getDate("regist_time");
+				String washed = rs.getString("washed");
+				String memo = rs.getString("memo");
+				String supervisor_chk = rs.getString("supervisor_chk");
+				int empno = rs.getInt("empno");
+				
+				DTO.setPh_num(ph_num);
+				DTO.setBody_temper(body_temper);
+				DTO.setRegist_time(regist_time);
+				DTO.setWashed(washed);
+				DTO.setMemo(memo);
+				DTO.setSupervisor_chk(supervisor_chk);
+				DTO.setEmpno(empno);
+				list.add(DTO);
+			}
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -106,10 +182,9 @@ public class PersonalHygieneDAO {
 			DataSource dataFactory = (DataSource) ctx.lookup("java:/comp/env/jdbc/charlie");
 //			System.out.println("DAOMODselect:"+dto.getMod());
 			conn = dataFactory.getConnection();
-				System.out.println("최상단DAO:"+dto.getMod());
 			
 			String query = "";
-			// 업데이트
+			// �뾽�뜲�씠�듃
 			if("up".equals(dto.getMod())) {
 				  query = "UPDATE personal_hygiene "
 						+ "SET ph_num = ?, "
@@ -121,14 +196,14 @@ public class PersonalHygieneDAO {
 						+ "memo = ? "
 						+ "where ph_num = ?";
 			}
-			// 인서트
+			// �씤�꽌�듃
 			if("add".equals(dto.getMod())) {
 				 query = "INSERT INTO personal_hygiene "
 					   + "(ph_num, empno, body_temper, regist_time, "
 					   + "washed, supervisor_chk, memo) "
-					   + "VALUES (?, ?, ?, SYSDATE, ?, ?, ?)";
+					   + "VALUES (hygiene_SEQ.nextval, ?, ?, SYSDATE, ?, ?, ?)";
 			}
-			// 딜리트
+			// �뵜由ы듃
 			if("delete".equals(dto.getMod())) {
 				query = "DELETE FROM personal_hygiene "
 					  + "WHERE ph_num = ?";
@@ -148,12 +223,11 @@ public class PersonalHygieneDAO {
 			
 			if("add".equals(dto.getMod())) {
 				System.out.println("addps");
-				ps.setInt(1, dto.getPh_num());
-				ps.setInt(2, dto.getEmpno());
-				ps.setDouble(3, dto.getBody_temper());
-				ps.setString(4, dto.getWashed());
-				ps.setString(5, dto.getSupervisor_chk());
-				ps.setString(6, dto.getMemo());
+				ps.setInt(1, dto.getEmpno());
+				ps.setDouble(2, dto.getBody_temper());
+				ps.setString(3, dto.getWashed());
+				ps.setString(4, dto.getSupervisor_chk());
+				ps.setString(5, dto.getMemo());
 			}
 			
 			if("delete".equals(dto.getMod())) {
@@ -193,4 +267,29 @@ public class PersonalHygieneDAO {
 		}
 		return result;
 	}
+	
+	public int getTotalCount() {
+
+		int total = 0;
+
+		try {
+			Context ctx = new InitialContext();
+			DataSource dataFactory = (DataSource) ctx.lookup("java:/comp/env/jdbc/charlie");
+
+			String query = "select count(*) from personal_hygiene";
+
+			try (Connection conn = dataFactory.getConnection();
+					PreparedStatement ps = conn.prepareStatement(query);
+					ResultSet rs = ps.executeQuery()) {
+
+				if (rs.next()) { 
+					total = rs.getInt(1);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return total;
+	}
+	
 }
--- charlie/src/main/webapp/WEB-INF/views/hygiene/hygieneDetail.jsp
+++ charlie/src/main/webapp/WEB-INF/views/hygiene/hygieneDetail.jsp
@@ -12,15 +12,15 @@
 <title>Insert title here</title>
 </head>
 <body>
-개인위생넘버 : ${hygiene[0].ph_num}<br>
-사원번호 : ${hygiene[0].empno}<br>
-체온 : ${hygiene[0].body_temper}<br>
-등록시간 : ${hygiene[0].regist_time}<br>
-위생체크 : ${hygiene[0].washed}<br>
-책임자체크 : ${hygiene[0].supervisor_chk}<br>
-메모 : ${hygiene[0].memo}<br>
-<a href="http://localhost:8080/charlie/hygiene?ph_num=${hygiene[0].ph_num}&mod=up">수정</a>
-<a href="http://localhost:8080/charlie/hygiene?ph_num=${hygiene[0].ph_num}&mod=delete">삭제</a>
+개인위생넘버 : ${map.list[0].ph_num}<br>
+사원번호 : ${map.list[0].empno}<br>
+체온 : ${map.list[0].body_temper}<br>
+등록시간 : ${map.list[0].regist_time}<br>
+위생체크 : ${map.list[0].washed}<br>
+책임자체크 : ${map.list[0].supervisor_chk}<br>
+메모 : ${map.list[0].memo}<br>
+<a href="http://localhost:8080/charlie/hygiene?ph_num=${map.list[0].ph_num}&mod=up">수정</a>
+<a href="http://localhost:8080/charlie/hygiene?ph_num=${map.list[0].ph_num}&mod=delete">삭제</a>
 
 </body>
 </html>
